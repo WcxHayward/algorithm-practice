2d24ce9a6836bdfc234b3937ede5fa67bec136cb
王椿喜 <hi_hayward@163.com>
2022-04-24 14:08:56 +0800

    Validate Binary Search Tree

--- ValidateBinarySearchTree/index.js
+++ ValidateBinarySearchTree/index.js
@@ -6,6 +6,11 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+  this.left = (left === undefined ? null : left)
+  this.right = (right === undefined ? null : right)
+}
 /**
  * @param {TreeNode} root
  * @return {boolean}
@@ -14,7 +19,7 @@ var isValidBST = function (root) {
   return isValid(root, -Infinity, Infinity)
 };
 var isValid = function (root, min, max) {
-  if (root === null) return true
+  if (!root) return true
   if (min !== null && root.val <= min) return false
   if (max !== null && root.val >= max) return false
   return isValid(root.left, min, root.val) && isValid(root.right, root.val, max)
